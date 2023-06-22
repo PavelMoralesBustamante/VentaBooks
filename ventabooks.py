e2ad6e8c649cc866ed6cd705a9009ae0e28fa243
PavelMoralesBustamante <pave.morales@profesor.duoc.cl>
2023-06-22 12:25:00 -0400

    Segundo commit menu1

--- ventabooks.py
+++ ventabooks.py
@@ -13,12 +13,23 @@ while True:
     #validamos la opción
     if opcion==0:
         fu.printv("Adios :D")
+        break
     elif opcion==1:
         fu.printv("Guardar")
     elif opcion==2:
         fu.printv("Buscar")
     elif opcion==3:
         fu.printv("Certificados")
+        print("1) Críticas")
+        print("2) Detalles")
+        cert = int(input("Seleccione : "))
+
+        if cert==1:
+            fu.printv("Certificado Críticas")
+        elif cert==2:
+            fu.printv("Certificado Detalle de ventas")
+        else:
+            fu.printr("Certificado no valido")
     else:
         fu.printr("Opción no valida")
 
